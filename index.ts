643ed83abd6c5f7be476ef8f023e01aed0110eb3
@ranjitsahoo873 <ranjitsahoo873@gmail.com>
2024-04-27 21:42:07 +0530

    staff changes

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    data: "Hello world",
+    data: "Hello world2222",
   });
 });
 
-app.listen(80, () => {
+app.listen(process.env.PORT || 80, () => {
   console.log("Server is listening on port 80");
 });
